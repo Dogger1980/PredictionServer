ef15f8902f74ac8c0619fce669fc9490fc41d935
Александр Родченко <rodhcenko-aleksandr01@mail.ru>
2025-10-27 10:35:14 +0300

    fixed bugs and enabled compression by default

--- nnpredictor/predictionserver/services.py
+++ nnpredictor/predictionserver/services.py
@@ -1,13 +1,13 @@
 from .loader import get_models
 from .predictor import predict
-from .processor import *
+from .processor import convert_input_data, convert_output_data
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
     
 def make_prediction(inputData):
     """Создает прогноз на основе входных данных.
     """
-    data = convert_input_data(inputData)
+    data = convert_input_data(inputData, compress=True)
     models = get_models()
     prediction = predict(models, data)
     return convert_output_data(prediction)
